9e9d1c0287ebf76ae1bdf86dae7ef5517f344444
sova777 <Sova777@users.noreply.github.com>
2010-05-25 00:23:46 +0400

    ???????? ?????? ?? Windows

--- AnalyzeFootballResults/nbproject/Package-Windows.bash
+++ AnalyzeFootballResults/nbproject/Package-Windows.bash
@@ -58,8 +58,8 @@ mkdir -p ${NBTMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory "${NBTMPDIR}/libAnalyzeFootballResults.dll/bin"
-copyFileToTmpDir "${OUTPUT_PATH}.exe" "${NBTMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}.exe" 0755
+makeDirectory "${NBTMPDIR}/libAnalyzeFootballResults.dll/lib"
+copyFileToTmpDir "${OUTPUT_PATH}" "${NBTMPDIR}/${PACKAGE_TOP_DIR}lib/${OUTPUT_BASENAME}" 0644
 
 
 # Generate tar file
