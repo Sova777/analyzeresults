390a3216d876e1a6402cb20dcb7ea206e3df1111
tester <tester@localhost>
2010-05-31 17:13:22 +0400

    ??????? ??????????? ?????? ?????????

---
+++ QAnalyzeFootballResults/nbproject/Makefile-Windows.mk
@@ -0,0 +1,78 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+GREP=grep
+NM=nm
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc.exe
+CCC=g++.exe
+CXX=g++.exe
+FC=
+AS=as.exe
+QMAKE=qmake.exe
+
+# Macros
+CND_PLATFORM=MinGW-Windows
+CND_CONF=Windows
+CND_DISTDIR=dist
+
+# Include project Makefile
+include Makefile
+
+# Object Directory
+OBJECTDIR=build/${CND_CONF}/${CND_PLATFORM}
+
+# Object Files
+OBJECTFILES=
+
+
+# C Compiler Flags
+CFLAGS=
+
+# CC Compiler Flags
+CCFLAGS=
+CXXFLAGS=
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Assembler Flags
+ASFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=-L../AnalyzeFootballResults/dist/Windows/MinGW-Windows -lAnalyzeFootballResults
+
+nbproject/qt-${CND_CONF}.mk: nbproject/qt-${CND_CONF}.pro FORCE
+	${QMAKE} VPATH=. -o qttmp-${CND_CONF}.mk nbproject/qt-${CND_CONF}.pro
+	mv -f qttmp-${CND_CONF}.mk nbproject/qt-${CND_CONF}.mk
+	@sed -e 's:\\\(.\):/\1:g' nbproject/qt-${CND_CONF}.mk >nbproject/qt-${CND_CONF}.tmp
+	@mv -f nbproject/qt-${CND_CONF}.tmp nbproject/qt-${CND_CONF}.mk
+
+FORCE:
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS} nbproject/qt-${CND_CONF}.mk
+	"${MAKE}" -f nbproject/qt-${CND_CONF}.mk dist/Windows/MinGW-Windows/QAnalyzeFootballResults.exe
+
+build/Windows/%.o: nbproject/qt-Windows.mk
+	$(MAKE) -f nbproject/qt-Windows.mk "$@"
+
+# Subprojects
+.build-subprojects:
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS} nbproject/qt-Windows.mk
+	$(MAKE) -f nbproject/qt-Windows.mk distclean
+
+# Subprojects
+.clean-subprojects:
---
+++ QAnalyzeFootballResults/nbproject/Makefile-variables.mk
@@ -0,0 +1,16 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Windows configuration
+CND_PLATFORM_Windows=MinGW-Windows
+CND_ARTIFACT_DIR_Windows=dist/Windows/MinGW-Windows
+CND_ARTIFACT_NAME_Windows=QAnalyzeFootballResults
+CND_ARTIFACT_PATH_Windows=dist/Windows/MinGW-Windows/QAnalyzeFootballResults
+CND_PACKAGE_DIR_Windows=dist/Windows/MinGW-Windows/package
+CND_PACKAGE_NAME_Windows=QAnalyzeFootballResults.tar
+CND_PACKAGE_PATH_Windows=dist/Windows/MinGW-Windows/package/QAnalyzeFootballResults.tar
