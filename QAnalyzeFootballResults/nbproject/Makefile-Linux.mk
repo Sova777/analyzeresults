7be796b2518606df59a9b812d2cbe889d012d21b
sova777 <sova777@localhost>
2010-06-01 11:04:24 +0400

    ?????? ???????????? "Linux"

---
+++ QAnalyzeFootballResults/nbproject/Makefile-Linux.mk
@@ -0,0 +1,78 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+GREP=grep
+NM=nm
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc
+CCC=g++
+CXX=g++
+FC=gfortran
+AS=as
+QMAKE=qmake
+
+# Macros
+CND_PLATFORM=GNU-Linux-x86
+CND_CONF=Linux
+CND_DISTDIR=dist
+
+# Include project Makefile
+include Makefile
+
+# Object Directory
+OBJECTDIR=build/${CND_CONF}/${CND_PLATFORM}
+
+# Object Files
+OBJECTFILES=
+
+
+# C Compiler Flags
+CFLAGS=
+
+# CC Compiler Flags
+CCFLAGS=
+CXXFLAGS=
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Assembler Flags
+ASFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=-Wl,-rpath ../AnalyzeFootballResults/dist/Linux/GNU-Linux-x86 -L../AnalyzeFootballResults/dist/Linux/GNU-Linux-x86 -lAnalyzeFootballResults
+
+nbproject/qt-${CND_CONF}.mk: nbproject/qt-${CND_CONF}.pro FORCE
+	${QMAKE} VPATH=. -o qttmp-${CND_CONF}.mk nbproject/qt-${CND_CONF}.pro
+	mv -f qttmp-${CND_CONF}.mk nbproject/qt-${CND_CONF}.mk
+
+FORCE:
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS} nbproject/qt-${CND_CONF}.mk
+	"${MAKE}" -f nbproject/qt-${CND_CONF}.mk dist/Linux/GNU-Linux-x86/QAnalyzeFootballResults
+
+build/Linux/%.o: nbproject/qt-Linux.mk
+	$(MAKE) -f nbproject/qt-Linux.mk "$@"
+
+# Subprojects
+.build-subprojects:
+	cd ../AnalyzeFootballResults && ${MAKE}  -f Makefile CONF=Linux
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS} nbproject/qt-Linux.mk
+	$(MAKE) -f nbproject/qt-Linux.mk distclean
+
+# Subprojects
+.clean-subprojects:
+	cd ../AnalyzeFootballResults && ${MAKE}  -f Makefile CONF=Linux clean
---
+++ QAnalyzeFootballResults/nbproject/qt-Linux.pro
@@ -0,0 +1,23 @@
+# This file is generated automatically. Do not edit.
+# Use project properties -> Build -> Qt -> Expert -> Custom Definitions.
+TEMPLATE = app
+DESTDIR = dist/Linux/GNU-Linux-x86
+TARGET = QAnalyzeFootballResults
+VERSION = 1.0.0
+CONFIG -= debug_and_release app_bundle lib_bundle
+CONFIG += debug 
+QT = core gui
+SOURCES += mainwindow.cpp main.cpp
+HEADERS += mainwindow.hpp
+FORMS +=
+RESOURCES +=
+TRANSLATIONS +=
+OBJECTS_DIR = build/Linux/GNU-Linux-x86
+MOC_DIR = 
+RCC_DIR = 
+UI_DIR = 
+QMAKE_CC = gcc
+QMAKE_CXX = g++
+DEFINES += 
+INCLUDEPATH += ../AnalyzeFootballResults 
+LIBS += -Wl,-rpath ../AnalyzeFootballResults/dist/Linux/GNU-Linux-x86 ../AnalyzeFootballResults/dist/Linux/GNU-Linux-x86/libAnalyzeFootballResults.so  
