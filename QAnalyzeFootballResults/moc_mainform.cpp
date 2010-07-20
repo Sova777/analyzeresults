3ab4c907e9c7e7b3e7095c11b1c6dbb4abb24e06
sova <sova@sova>
2010-07-20 21:26:16 +0400

    ??????? ???????? ????? ? ???????

--- QAnalyzeFootballResults/moc_mainform.cpp
+++ QAnalyzeFootballResults/moc_mainform.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'mainform.h'
 **
-** Created: Tue Jul 20 20:57:41 2010
+** Created: Tue Jul 20 21:09:15 2010
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.6.2)
 **
 ** WARNING! All changes made in this file will be lost!
