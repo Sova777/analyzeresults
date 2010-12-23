637a2fc869a645bd5c00bd65871b5ccc0f65ac1b
valera <valera@sova>
2010-12-24 00:35:11 +0300

    ??????? ????????? ???????

--- QAnalyzeFootballResults/nbproject/Makefile-Windows.mk
+++ QAnalyzeFootballResults/nbproject/Makefile-Windows.mk
@@ -14,16 +14,16 @@ GREP=grep
 NM=nm
 CCADMIN=CCadmin
 RANLIB=ranlib
-CC=cc
-CCC=CC
-CXX=CC
-FC=f90
-AS=as
+CC=gcc.exe
+CCC=g++.exe
+CXX=g++.exe
+FC=gfortran
+AS=as.exe
 PROC=proc
-QMAKE=qmake
+QMAKE=qmake.exe
 
 # Macros
-CND_PLATFORM=OracleSolarisStudio-Solaris-x86
+CND_PLATFORM=MinGW-Windows
 CND_CONF=Windows
 CND_DISTDIR=dist
 
@@ -51,17 +51,19 @@ FFLAGS=
 ASFLAGS=
 
 # Link Libraries and Options
-LDLIBSOPTIONS=../AnalyzeFootballResults/dist/Windows/OracleSolarisStudio-Solaris-x86/libAnalyzeFootballResults.dll
+LDLIBSOPTIONS=-L../AnalyzeFootballResults/dist/Windows/MinGW-Windows -lAnalyzeFootballResults
 
 nbproject/qt-${CND_CONF}.mk: nbproject/qt-${CND_CONF}.pro FORCE
-	${QMAKE} VPATH=. -o qttmp-${CND_CONF}.mk nbproject/qt-${CND_CONF}.pro
+	${QMAKE} VPATH=. -spec win32-g++ -o qttmp-${CND_CONF}.mk nbproject/qt-${CND_CONF}.pro
 	mv -f qttmp-${CND_CONF}.mk nbproject/qt-${CND_CONF}.mk
+	@sed -e 's/\/qt\/bin/\/qt\/bin\//g' nbproject/qt-${CND_CONF}.mk >nbproject/qt-${CND_CONF}.tmp
+	@mv -f nbproject/qt-${CND_CONF}.tmp nbproject/qt-${CND_CONF}.mk
 
 FORCE:
 
 # Build Targets
 .build-conf: ${BUILD_SUBPROJECTS} nbproject/qt-${CND_CONF}.mk
-	"${MAKE}" -f nbproject/qt-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/QAnalyzeFootballResults
+	"${MAKE}" -f nbproject/qt-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/QAnalyzeFootballResults.exe
 
 ${CND_BUILDDIR}/Windows/%.o: nbproject/qt-${CND_CONF}.mk
 	$(MAKE) -f nbproject/qt-${CND_CONF}.mk "$@"
--- QAnalyzeFootballResults/nbproject/qt-Windows.pro
+++ QAnalyzeFootballResults/nbproject/qt-Windows.pro
@@ -1,7 +1,7 @@
 # This file is generated automatically. Do not edit.
 # Use project properties -> Build -> Qt -> Expert -> Custom Definitions.
 TEMPLATE = app
-DESTDIR = dist/Windows/OracleSolarisStudio-Solaris-x86
+DESTDIR = dist/Windows/MinGW-Windows
 TARGET = QAnalyzeFootballResults
 VERSION = 1.0.0
 CONFIG -= debug_and_release app_bundle lib_bundle
@@ -12,12 +12,12 @@ HEADERS += mainform.h
 FORMS += mainform.ui
 RESOURCES +=
 TRANSLATIONS +=
-OBJECTS_DIR = build/Windows/OracleSolarisStudio-Solaris-x86
+OBJECTS_DIR = build/Windows/MinGW-Windows
 MOC_DIR = 
 RCC_DIR = 
 UI_DIR = 
-QMAKE_CC = cc
-QMAKE_CXX = CC
+QMAKE_CC = gcc.exe
+QMAKE_CXX = g++.exe
 DEFINES += 
 INCLUDEPATH += ../AnalyzeFootballResults 
-LIBS += -R../AnalyzeFootballResults/dist/Windows/OracleSolarisStudio-Solaris-x86 ../AnalyzeFootballResults/dist/Windows/OracleSolarisStudio-Solaris-x86/libAnalyzeFootballResults.dll  
+LIBS += -L../AnalyzeFootballResults/dist/Windows/MinGW-Windows ../AnalyzeFootballResults/dist/Windows/MinGW-Windows/libAnalyzeFootballResults.dll  
