ba64d7d726ea944fdab1f326bcda86e0f9af0a43
sova777 <Sova777@users.noreply.github.com>
2010-12-24 00:50:17 +0300

    ??????? ????????? ???????

--- AnalyzeResultsProgram/nbproject/Makefile-variables.mk
+++ AnalyzeResultsProgram/nbproject/Makefile-variables.mk
@@ -15,21 +15,21 @@ CND_PACKAGE_DIR_Solaris=dist/Solaris//package
 CND_PACKAGE_NAME_Solaris=analyzeresultsprogram.tar
 CND_PACKAGE_PATH_Solaris=dist/Solaris//package/analyzeresultsprogram.tar
 # Linux configuration
-CND_PLATFORM_Linux=
-CND_ARTIFACT_DIR_Linux=dist/Linux/
+CND_PLATFORM_Linux=GNU-Linux-x86
+CND_ARTIFACT_DIR_Linux=dist/Linux/GNU-Linux-x86
 CND_ARTIFACT_NAME_Linux=analyzeresultsprogram
-CND_ARTIFACT_PATH_Linux=dist/Linux//analyzeresultsprogram
-CND_PACKAGE_DIR_Linux=dist/Linux//package
+CND_ARTIFACT_PATH_Linux=dist/Linux/GNU-Linux-x86/analyzeresultsprogram
+CND_PACKAGE_DIR_Linux=dist/Linux/GNU-Linux-x86/package
 CND_PACKAGE_NAME_Linux=analyzeresultsprogram.tar
-CND_PACKAGE_PATH_Linux=dist/Linux//package/analyzeresultsprogram.tar
+CND_PACKAGE_PATH_Linux=dist/Linux/GNU-Linux-x86/package/analyzeresultsprogram.tar
 # Windows configuration
-CND_PLATFORM_Windows=MinGW-Windows
-CND_ARTIFACT_DIR_Windows=dist/Windows/MinGW-Windows
+CND_PLATFORM_Windows=
+CND_ARTIFACT_DIR_Windows=dist/Windows/
 CND_ARTIFACT_NAME_Windows=analyzeresultsprogram
-CND_ARTIFACT_PATH_Windows=dist/Windows/MinGW-Windows/analyzeresultsprogram
-CND_PACKAGE_DIR_Windows=dist/Windows/MinGW-Windows/package
+CND_ARTIFACT_PATH_Windows=dist/Windows//analyzeresultsprogram
+CND_PACKAGE_DIR_Windows=dist/Windows//package
 CND_PACKAGE_NAME_Windows=analyzeresultsprogram.tar
-CND_PACKAGE_PATH_Windows=dist/Windows/MinGW-Windows/package/analyzeresultsprogram.tar
+CND_PACKAGE_PATH_Windows=dist/Windows//package/analyzeresultsprogram.tar
 #
 # include compiler specific variables
 #
