73d2317aacf9459ecb670529c96eda71640571e9
sova777 <sova777@localhost>
2011-03-07 13:24:33 +0300

    ?????? ????? ??????

---
+++ AnalyzeReport/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: sova
+ *
+ * Created on 7 Март 2011 г., 13:22
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
