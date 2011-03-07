ab257e1aa908ff81ce3619edb74f04cfbc2a0d06
valera <Sova777@users.noreply.github.com>
2011-03-07 19:28:33 +0300

    ?????????? ????????

--- QAnalyzeFootballResults/nbproject/Makefile-variables.mk
+++ QAnalyzeFootballResults/nbproject/Makefile-variables.mk
@@ -7,29 +7,29 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Solaris configuration
-CND_PLATFORM_Solaris=OracleSolarisStudio-Linux-x86
-CND_ARTIFACT_DIR_Solaris=dist/Solaris/OracleSolarisStudio-Linux-x86
+CND_PLATFORM_Solaris=
+CND_ARTIFACT_DIR_Solaris=dist/Solaris/
 CND_ARTIFACT_NAME_Solaris=QAnalyzeFootballResults
-CND_ARTIFACT_PATH_Solaris=dist/Solaris/OracleSolarisStudio-Linux-x86/QAnalyzeFootballResults
-CND_PACKAGE_DIR_Solaris=dist/Solaris/OracleSolarisStudio-Linux-x86/package
+CND_ARTIFACT_PATH_Solaris=dist/Solaris//QAnalyzeFootballResults
+CND_PACKAGE_DIR_Solaris=dist/Solaris//package
 CND_PACKAGE_NAME_Solaris=QAnalyzeFootballResults.tar
-CND_PACKAGE_PATH_Solaris=dist/Solaris/OracleSolarisStudio-Linux-x86/package/QAnalyzeFootballResults.tar
+CND_PACKAGE_PATH_Solaris=dist/Solaris//package/QAnalyzeFootballResults.tar
 # Linux configuration
-CND_PLATFORM_Linux=GNU-Linux-x86
-CND_ARTIFACT_DIR_Linux=dist/Linux/GNU-Linux-x86
+CND_PLATFORM_Linux=
+CND_ARTIFACT_DIR_Linux=dist/Linux/
 CND_ARTIFACT_NAME_Linux=QAnalyzeFootballResults
-CND_ARTIFACT_PATH_Linux=dist/Linux/GNU-Linux-x86/QAnalyzeFootballResults
-CND_PACKAGE_DIR_Linux=dist/Linux/GNU-Linux-x86/package
+CND_ARTIFACT_PATH_Linux=dist/Linux//QAnalyzeFootballResults
+CND_PACKAGE_DIR_Linux=dist/Linux//package
 CND_PACKAGE_NAME_Linux=QAnalyzeFootballResults.tar
-CND_PACKAGE_PATH_Linux=dist/Linux/GNU-Linux-x86/package/QAnalyzeFootballResults.tar
+CND_PACKAGE_PATH_Linux=dist/Linux//package/QAnalyzeFootballResults.tar
 # Windows configuration
-CND_PLATFORM_Windows=
-CND_ARTIFACT_DIR_Windows=dist/Windows/
+CND_PLATFORM_Windows=MinGW-Windows
+CND_ARTIFACT_DIR_Windows=dist/Windows/MinGW-Windows
 CND_ARTIFACT_NAME_Windows=QAnalyzeFootballResults
-CND_ARTIFACT_PATH_Windows=dist/Windows//QAnalyzeFootballResults
-CND_PACKAGE_DIR_Windows=dist/Windows//package
+CND_ARTIFACT_PATH_Windows=dist/Windows/MinGW-Windows/QAnalyzeFootballResults
+CND_PACKAGE_DIR_Windows=dist/Windows/MinGW-Windows/package
 CND_PACKAGE_NAME_Windows=QAnalyzeFootballResults.tar
-CND_PACKAGE_PATH_Windows=dist/Windows//package/QAnalyzeFootballResults.tar
+CND_PACKAGE_PATH_Windows=dist/Windows/MinGW-Windows/package/QAnalyzeFootballResults.tar
 #
 # include compiler specific variables
 #
