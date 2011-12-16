34afa194efcffe77849505e215ffea9ea356983e
valera <Sova777@users.noreply.github.com>
2011-12-17 00:05:32 +0400

    ???????? ???? ?? 2 ?????

---
+++ QFootballStat/XmlFileReader.h
@@ -0,0 +1,54 @@
+/*
+Copyright (c) 2009 - 2011, Valeriy Soldatov
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+ * Neither the name of the football.mojgorod.ru nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef XMLFILEREADER_H
+#define	XMLFILEREADER_H
+
+#include <QtGui>
+#include <QtXml>
+
+typedef QString StatHashKey;
+typedef Record StatHashValue;
+typedef QHash<StatHashKey, StatHashValue*> StatHash;
+typedef void (*pointer)(const QDomElement& docElement, StatHash* hash);
+
+void listOfGoals(const QDomElement& docElement, StatHash* hash);
+void listOfMatches(const QDomElement& docElement, StatHash* hash);
+void listOfReferies(const QDomElement& docElement, StatHash* hash);
+void listOfTable(const QDomElement& docElement, StatHash* hash);
+void listOfCoaches(const QDomElement& docElement, StatHash* hash);
+void listOfStadiums(const QDomElement& docElement, StatHash* hash);
+void listOfStadiums2(const QDomElement& docElement, StatHash* hash);
+void listOfPlayers(const QDomElement& docElement, StatHash* hash);
+
+QString getTeam1(const QDomElement& docElement);
+QString getTeam2(const QDomElement& docElement);
+QDate getDate(const QDomElement& docElement);
+QString getScore(const QDomElement& docElement);
+QString getStadium(const QDomElement& docElement, QString* city = NULL);
+
+#endif	/* XMLFILEREADER_H */
