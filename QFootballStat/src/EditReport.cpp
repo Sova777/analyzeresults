184bb557532e64e5b2bcef0935f33f11f9204e80
valera <valera@sova>
2012-07-08 22:37:23 +0400

    ????????? ? ?????  "????????????? ?????"

--- QFootballStat/src/EditReport.cpp
+++ QFootballStat/src/EditReport.cpp
@@ -26,10 +26,94 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #include "EditReport.h"
+#include "Report.h"
 
 EditReport::EditReport(MainWindow *parent) {
     mainWindow = parent;
     widget.setupUi(this);
+    int size = mainWindow->current.length();
+    if (size > 0) {
+        QString code = mainWindow->current.left(5);
+        QString id = mainWindow->current.right(size - 5);
+        if (code == "xm01_") {
+            foreach(Report report, mainWindow->reports) {
+                if (report.getFileName() == id) {
+                    this->widget.fChampionship->setText(report.getMatchTournament());
+                    this->widget.fRound->setText(report.getMatchRound());
+                    this->widget.fTeam1->setText(report.getTeam1());
+                    this->widget.fTeam2->setText(report.getTeam2());
+                    this->widget.fCity->setText(report.getStadiumCity());
+                    this->widget.fStadium->setText(report.getStadium());
+                    this->widget.fAttendance->setText(report.getStadiumAttendance());
+                    this->widget.fReferee->setText(report.getReferee());
+                    this->widget.fDate->setDate(report.getDate());
+                    QTime time = QTime::fromString(report.getTime());
+                    this->widget.fDate->setTime(time);
+                    this->widget.fCoach1->setText(report.getCoach1());
+                    this->widget.fCoach2->setText(report.getCoach2());
+                    QVector<Report::Player> players1 = report.getPlayers1();
+                    this->widget.fSquad1->setColumnCount(1);
+                    QStringList titles1;
+                    titles1 << TABLE_SQUAD_COLUMN1;
+                    this->widget.fSquad1->setHorizontalHeaderLabels(titles1);
+                    this->widget.fSquad1->setRowCount(players1.size());
+                    int row = 0;
+                    foreach(Report::Player player1, players1) {
+                        QTableWidgetItem* stlb = new QTableWidgetItem(player1.player);
+                        stlb->setFlags(Qt::ItemIsSelectable | Qt::ItemIsEnabled);
+                        this->widget.fSquad1->setItem(row, 0, stlb);
+                        row++;
+                    }
+                    QVector<Report::Player> players2 = report.getPlayers2();
+                    this->widget.fSquad2->setColumnCount(1);
+                    QStringList titles2;
+                    titles2 << TABLE_SQUAD_COLUMN1;
+                    this->widget.fSquad2->setHorizontalHeaderLabels(titles2);
+                    this->widget.fSquad2->setRowCount(players2.size());
+                    row = 0;
+                    foreach(Report::Player player2, players2) {
+                        QTableWidgetItem* stlb = new QTableWidgetItem(player2.player);
+                        stlb->setFlags(Qt::ItemIsSelectable | Qt::ItemIsEnabled);
+                        this->widget.fSquad2->setItem(row, 0, stlb);
+                        row++;
+                    }
+                    QVector<Report::Event> events = report.getEvents();
+                    this->widget.fEvents->setColumnCount(6);
+                    QStringList titles3;
+                    titles3 << TABLE_EVENT_COLUMN1
+                            << TABLE_EVENT_COLUMN2
+                            << TABLE_EVENT_COLUMN3
+                            << TABLE_EVENT_COLUMN4
+                            << TABLE_EVENT_COLUMN5
+                            << TABLE_EVENT_COLUMN6;
+                    this->widget.fEvents->setHorizontalHeaderLabels(titles3);
+                    this->widget.fEvents->setRowCount(events.size());
+                    row = 0;
+                    foreach(Report::Event event, events) {
+                        QTableWidgetItem* stlb1 = new QTableWidgetItem(event.time);
+                        stlb1->setFlags(Qt::ItemIsSelectable | Qt::ItemIsEnabled);
+                        this->widget.fEvents->setItem(row, 0, stlb1);
+                        QTableWidgetItem* stlb2 = new QTableWidgetItem(event.type);
+                        stlb2->setFlags(Qt::ItemIsSelectable | Qt::ItemIsEnabled);
+                        this->widget.fEvents->setItem(row, 1, stlb2);
+                        QTableWidgetItem* stlb3 = new QTableWidgetItem(event.comment);
+                        stlb3->setFlags(Qt::ItemIsSelectable | Qt::ItemIsEnabled);
+                        this->widget.fEvents->setItem(row, 2, stlb3);
+                        QTableWidgetItem* stlb4 = new QTableWidgetItem(event.team);
+                        stlb4->setFlags(Qt::ItemIsSelectable | Qt::ItemIsEnabled);
+                        this->widget.fEvents->setItem(row, 3, stlb4);
+                        QTableWidgetItem* stlb5 = new QTableWidgetItem(event.player);
+                        stlb5->setFlags(Qt::ItemIsSelectable | Qt::ItemIsEnabled);
+                        this->widget.fEvents->setItem(row, 4, stlb5);
+                        QTableWidgetItem* stlb6 = new QTableWidgetItem(event.player2);
+                        stlb6->setFlags(Qt::ItemIsSelectable | Qt::ItemIsEnabled);
+                        this->widget.fEvents->setItem(row, 5, stlb6);
+                        row++;
+                    }
+                }
+            }
+        }
+    }
 }
 
 EditReport::~EditReport() {
