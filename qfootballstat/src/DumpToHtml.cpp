91021aa1989afbf7d916b51fe8de36ce5f4b5987
valera <Sova777@users.noreply.github.com>
2015-06-07 18:25:01 +0300

    ????????? ????? ??? HTML ????

--- qfootballstat/src/DumpToHtml.cpp
+++ qfootballstat/src/DumpToHtml.cpp
@@ -26,39 +26,118 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #include "DumpToHtml.h"
+#include "MainWindow.h"
 #include <QFile>
 #include <QTextStream>
 
-void DumpToHtml::save(QString directory) {
-    QString qstr = directory + "/referies.html";
-    QFile file(qstr);
-    if (!file.open(QIODevice::WriteOnly | QIODevice::Text)) return;
-    QTextStream out(&file);
+void DumpToHtml::save(QVector<Report>& reports, QString directory) {
+    int id = 0;
+    foreach(Report report, reports) {
+        QString qstr = QString::fromLatin1("%1/report_%2.html").arg(directory).arg(id);
+        QFile file(qstr);
+        if (file.open(QIODevice::WriteOnly | QIODevice::Text)) {
+            QTextStream out(&file);
+            QString team1 = report.getTeam1();
+            QString team2 = report.getTeam2();
+            QString coach1 = report.getCoach1();
+            QString coach2 = report.getCoach2();
+            const QString* score = report.getScore();
+            QString time = report.getTime();
+            QDate date = report.getDate();
+            QString rlocation = report.getRefereeLocation();
+            QString referee = report.getReferee();
+            QString round = report.getMatchRound();
+            QString tournament = report.getMatchTournament();
+            QString city = report.getStadiumCity();
+            QString attendance = report.getStadiumAttendance();
+            QString stadium = report.getStadium();
+            QString text = QString::fromUtf8("<h1 align='center'>%1</h1>")
+                    .arg(tournament);
+            if ((round == "") || (round.length() > 3)) {
+                text.append(QString::fromUtf8("<h2 align='center'>%1</h2>")
+                        .arg(round));
+            } else {
+                text.append(QString::fromUtf8("<h2 align='center'>Тур: %1</h2>")
+                        .arg(round));
+            }
+            text.append(QString::fromUtf8("<h2 align='center'>%1 - %2 - %3</h2>")
+                    .arg(team1)
+                    .arg(team2)
+                    .arg(*score));
+            text.append(QString::fromUtf8("<p>"));
+            if (city != "") {
+                text.append(QString::fromUtf8("%1. ").arg(city));
+            }
+            text.append(QString::fromUtf8("%1. %2 зрителей<br><b>Судья:</b> %3 (%4)<br>%6 %5</p>")
+                    .arg(stadium)
+                    .arg(attendance)
+                    .arg(referee)
+                    .arg(rlocation)
+                    .arg(time)
+                    .arg(date.toString(Qt::DefaultLocaleLongDate)));
 
-//    if (reports.size() > 0) {
-//        Report report = reports[0];
-//        XmlFilter xmlFilter(this, &report, report.getReportId(), &filter);
-//        (*func)(xmlFilter, &hash);
-//    }
+            // игроки
+            QVector<Report::Player> players1 = report.getPlayers1();
+            text.append(QString::fromLatin1("<table border='0' width='100%'>"));
+            text.append(QString::fromLatin1("<tr><td>"));
+            uint length1 = players1.size();
+            for (uint i = 0; i < length1; i++) {
+                QString value = players1.at(i).player;
+                if (i != 0) {
+                    text.append(QString("<br>"));
+                }
+                text.append(QString("%1").arg(value));
+            }
+            text.append(QString::fromLatin1("</td><td>"));
+            QVector<Report::Player> players2 = report.getPlayers2();
+            uint length2 = players2.size();
+            for (uint i = 0; i < length2; i++) {
+                QString value = players2.at(i).player;
+                if (i != 0) {
+                    text.append(QString("<br>"));
+                }
+                text.append(QString("%1").arg(value));
+            }
+            text.append(QString::fromLatin1("</td></tr>"));
+            text.append(QString::fromUtf8("<tr><td><b>Тренер:</b> %1</td><td><b>Тренер:</b> %2</td></tr>").arg(coach1).arg(coach2));
+            text.append(QString::fromLatin1("</table>"));
 
-    out << "| ";
-//    for (int k = 0; k < columns; k++) {
-//        if (!widget.table->isColumnHidden(k)) {
-//            QString label = widget.table->horizontalHeaderItem(k)->text();
-//            int len = widget.table->columnWidth(k);
-//            out << label.replace("\n", "").rightJustified(len / 7, ' ', true) << " | ";
-//        }
-//    }
-    out << "\n";
-//    for (int i = 0; i < rows; i++) {
-//        out << "| ";
-//        for (int j = 0; j < columns; j++) {
-//            if (!widget.table->isColumnHidden(j)) {
-//                int len = widget.table->columnWidth(j);
-//                out << widget.table->item(i, j)->text().rightJustified(len / 7, ' ', false) << " | ";
-//            }
-//        }
-//        out << "\n";
-//    }
-    file.close();
+            // события в матче
+            text.append(QString::fromLatin1("<table border='0' width='100%'>"));
+            QVector<Report::Event> events = report.getEvents();
+            uint length = events.size();
+            for (uint i = 0; i < length; i++) {
+                QString eventType = events.at(i).type;
+                QString player = events.at(i).player;
+                QString player2 = events.at(i).player2;
+                QString team = events.at(i).team;
+                QString time = events.at(i).time;
+                if (eventType == EVENT_GOAL) {
+                    text.append(QString("<tr><td>%1'</td><td>%2 %3</td><td>%4</td><td>%5</td></tr>").arg(time).arg("<img src='images/goal.png'>").arg(eventType).arg(team).arg(player));
+                } else if (eventType == EVENT_GOAL_PENALTY) {
+                    text.append(QString("<tr><td>%1'</td><td>%2 %3</td><td>%4</td><td>%5</td></tr>").arg(time).arg("<img src='images/pen.png'>").arg(eventType).arg(team).arg(player));
+                } else if (eventType == EVENT_MISSED_PENALTY) {
+                    text.append(QString("<tr><td>%1'</td><td>%2 %3</td><td>%4</td><td>%5</td></tr>").arg(time).arg("<img src='images/missed.png'>").arg(eventType).arg(team).arg(player));
+                } else if (eventType == EVENT_AUTOGOAL) {
+                    text.append(QString("<tr><td>%1'</td><td>%2 %3</td><td>%4</td><td>%5</td></tr>").arg(time).arg("<img src='images/autogoal.png'>").arg(eventType).arg(team).arg(player));
+                } else if (eventType == EVENT_RED_CARD) {
+                    text.append(QString("<tr><td>%1'</td><td>%2 %3</td><td>%4</td><td>%5</td></tr>").arg(time).arg("<img src='images/red.png'>").arg(eventType).arg(team).arg(player));
+                } else if (eventType == EVENT_RED_YELLOW_CARD) {
+                    text.append(QString("<tr><td>%1'</td><td>%2 %3</td><td>%4</td><td>%5</td></tr>").arg(time).arg("<img src='images/yellow2.png'>").arg(eventType).arg(team).arg(player));
+                } else if (eventType == EVENT_YELLOW_CARD) {
+                    text.append(QString("<tr><td>%1'</td><td>%2 %3</td><td>%4</td><td>%5</td></tr>").arg(time).arg("<img src='images/yellow.png'>").arg(eventType).arg(team).arg(player));
+                } else if (eventType == EVENT_SUBSTITUTION) {
+                    text.append(QString("<tr><td>%1'</td><td>%2 %3</td><td>%4</td><td>%5 - %6</td></tr>").arg(time).arg("<img src='images/subs.png'>").arg(eventType).arg(team).arg(player).arg(player2));
+                } else if (player2 == "") {
+                    text.append(QString("<tr><td>%1'</td><td>%2</td><td>%3</td><td>%4</td></tr>").arg(time).arg(eventType).arg(team).arg(player));
+                } else {
+                    text.append(QString("<tr><td>%1'</td><td>%2</td><td>%3</td><td>%4 - %5</td></tr>").arg(time).arg(eventType).arg(team).arg(player).arg(player2));
+                }
+            }
+            text.append(QString::fromLatin1("</table>"));
+            out << text;
+            file.close();
+        }
+        id++;
+    }
 }
